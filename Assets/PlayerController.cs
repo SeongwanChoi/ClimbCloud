d0c95989ef79d4747a165f4121e63983e3770f38
Choi Seong-wan <75936092+SeongwanChoi@users.noreply.github.com>
2023-04-17 18:16:08 +0900

    배포 테스트 완료

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
 
     void Start()
     {
@@ -20,15 +22,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // 점프한다
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
+            
 
         // 좌우 이동
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < this.threshold) key = -1;
 
         // 플레이어의 속도
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
@@ -42,11 +46,24 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(key, 1, 1);
 
         // 플레이어 속도에 맞춰 애니메이션 속도를 바꾼다.
-        this.animator.speed = speedx / maxWalkSpeed;
+        if (true)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+        
+
+        // 플레이어가 화면 밖으로 나갔다면 처음부터
+        if (transform.position.y < -10)
+            SceneManager.LoadScene("GameScene");
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("골");    
+        Debug.Log("골");
+        SceneManager.LoadScene("ClearScene");
     }
 }
